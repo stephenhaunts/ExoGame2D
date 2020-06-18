198722040a8db51d3c3973c7edc8981bdff73c40
Stephen Haunts <stephen.haunts@outlook.com>
2020-06-18 11:30:06 +0100

    Add in code for Tutorial 1

---
+++ Tutorials/Tutorial-1-Blank Screen/GameLoop.cs
@@ -0,0 +1,68 @@
+﻿/*
+MIT License
+
+Copyright (c) 2020
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using GameEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ExoGameEngine.Tutorials.Tutorial1_BlankScreen
+{
+    public class GameLoop : Game
+    {
+        public GameLoop()
+        {
+            Engine.InitializeEngine(this, 1280, 720);
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Window.AllowUserResizing = true;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            InputHelper.Update();
+
+            if (InputHelper.KeyPressed(Keys.Escape))
+            {
+                Exit();         
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {      
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
